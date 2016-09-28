fabb13616966bc98e52f8e51cc2ef9f22d11196c
Deepak <grc.deepak@gmail.com>
2016-09-28 13:44:50 -0700

    validation for required questions

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,20 +3,16 @@ class ResponsesController < ApplicationController
     new_response = Response.new(name: params["response"]["name"])
 
     new_response.answers_attributes= whitelisted_answer_params
-    if validate_response(params) && new_response.save
+    if validate_response && new_response.save
       flash[:success] = "Thank you for taking the survey"
       redirect_to :root
     else
       @survey = Survey.find(params["response"]["survey_id"])
       @questions = @survey.questions
       @response = Response.new
-      flash.now[:danger] = "Please correct the errors and resubmit"
+      flash.now[:danger] = "Some of the questions are mandatory, please resubmit"
       render 'questions/index'
     end
-  end
-
-  def index
-
   end
 
   private
@@ -31,7 +27,14 @@ class ResponsesController < ApplicationController
     temp_arr
   end
 
-  def validate_response(params)
-    true
+  def validate_response
+    @survey =  Survey.find(params["response"]["survey_id"])
+    required_questions = @survey.questions.where(required: true).pluck(:id)
+    answered_questions = whitelisted_answer_params.map { |a| a[:question_id].to_i }
+    if (required_questions - answered_questions).empty?
+      true
+    else
+      false
+    end
   end
 end
